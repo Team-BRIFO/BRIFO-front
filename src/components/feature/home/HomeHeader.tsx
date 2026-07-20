e13de60ff623201a641cb16f0afc6efe50a616d6
bini0918 <165636716+bini0918@users.noreply.github.com>
2026-07-20 22:28:25 +0900

    feat: 홈 헤더 컴포넌트 구현

---
+++ src/components/feature/home/HomeHeader.tsx
@@ -0,0 +1,19 @@
+interface HomeHeaderProps {
+  nickname: string
+  companyName: string
+}
+
+export default function HomeHeader({ nickname, companyName }: HomeHeaderProps) {
+  return (
+    <section className="flex items-end justify-between py-3">
+      <div>
+        <h2 className="dnf-Subtitle2 text-Yellow-40">{nickname}님</h2>
+        <p className="dnf-Subtitle2 text-Gray-10 mt-1.5">출근하셨네요!</p>
+      </div>
+
+      <div className="pretendard-Caption1 bg-Gray-1 text-Gray-5 rounded-full px-4 py-2">
+        {companyName}
+      </div>
+    </section>
+  )
+}
