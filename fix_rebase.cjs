b1ee489e9e9c373fa3cbcc30ecf6ebba37a58cb1
haewonwon <khcw1029@daum.net>
2026-07-28 18:47:55 +0900

    chore: 임시 패치/리베이스 스크립트 파일 정리

--- fix_rebase.cjs
+++
@@ -1,9 +0,0 @@
-﻿const fs = require('fs')
-const file = process.argv[2]
-let content = fs.readFileSync(file, 'utf8')
-if (file.includes('git-rebase-todo')) {
-  content = content.replace(/^pick 7534e99/m, 'reword 7534e99')
-} else if (file.includes('COMMIT_EDITMSG')) {
-  content = content.replace(/^design:/, 'style:')
-}
-fs.writeFileSync(file, content)
